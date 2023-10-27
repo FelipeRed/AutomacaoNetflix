ce11fd26f3e496bfe4503d6a8aa27a89cf7f8d48
Felipe Vermelho Martins <86678984+FelipeRed@users.noreply.github.com>
2023-10-27 18:28:32 -0300

    Otimizando o algoritmo

--- program/auto.py
+++ program/auto.py
@@ -1,33 +1,24 @@
 import pyautogui
 import time
 
-def pular_aberturas(iteracoes):
-    # Essa função pula a abertura de no máximo 5 episódios
-    if iteracoes == 5:
-        return
-
-    acao_realizada = ""
-    while acao_realizada != "Pular Abertura":
-        # Tenta localizar a imagem na tela (capturando o seu centro)
-        botao = pyautogui.locateCenterOnScreen("btn_PularAbertura.png", confidence = 0.7)
-        if botao is not None:
-            acao_realizada = "Pular Abertura"
-            pyautogui.click(botao.x, botao.y)
-            continue
-
-        botao = pyautogui.locateCenterOnScreen("btn_PularResumo.png", confidence = 0.7)
-        if botao is not None:
-            pyautogui.click(botao.x, botao.y)
-            print("Botão de Pular Resumo encontrado.")
-            continue
-
-        print("Buscando...")
-        time.sleep(1)
-
-    print("Botão de Pular Abertura encontrado.")
-    print("Sleep de 10 minutos.")
-    time.sleep(600)  # Espera 10 minutos até chamar a função recursivamente
-    pular_aberturas(iteracoes + 1)
+# pyautogui.locateCenterOnScreen tenta localizar a imagem na tela (capturando o seu centro)
+# Confidence é um parâmetro que determina quão igual a imagem encontrada deve ser (nesse caso 70%)
+def pular_aberturas(n_vezes):  # n_vezes é o número de aberturas que o código irá pular
+    imagens = ["btn_PularAbertura.png", "btn_PularResumo.png"]
+    for i in range(n_vezes):
+        abertura_pulada = False
+        while not abertura_pulada:
+            for img in imagens:
+                botao = pyautogui.locateCenterOnScreen(img, confidence=0.7)
+                if botao is not None:
+                    pyautogui.click(botao.x, botao.y)
+                    if img == "btn_PularAbertura.png":
+                        abertura_pulada = True
+                print("Buscando...")
+                time.sleep(1)
+        print("Botão de Pular Abertura encontrado.")
+        print("Sleep de 10 minutos.")
+        time.sleep(600)
 
 # "Main"
-pular_aberturas(0)
+pular_aberturas(5)
